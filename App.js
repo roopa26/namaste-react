72e03b31774cbf2964cb3d3463bfd2ed21e96c34
roopa26 <roopashree65@gmail.com>
2024-07-12 12:34:15 -1200

    episode 1

---
+++ App.js
@@ -0,0 +1,4 @@
+var heading = React.createElement("h1",{id:"heading"},"Hello World from App!");
+console.log(heading);
+        var root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
