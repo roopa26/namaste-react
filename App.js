6e75e1e75f32a7ac41a6253adb0b366df99efc22
roopa26 <roopashree65@gmail.com>
2024-07-17 10:07:19 -1200

    Igniting our app

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
-var heading = React.createElement("h1",{id:"heading"},"Hello World from App!");
-console.log(heading);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+var jsxHeading = <h1 id="jxsHeading">Namaste react using jsx</h1>
+
+var heading = React.createElement("h1",{id:"heading"},"Namaste React");
+//console.log(heading);
         var root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+        root.render(jsxHeading);
