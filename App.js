2c674e858546286c86cfb6906b462ea31a138a7c
roopa26 <roopashree65@gmail.com>
2024-07-17 11:08:52 -1200

    Laying the foundation-functional component introduction

--- App.js
+++ App.js
@@ -4,6 +4,15 @@ import ReactDOM from 'react-dom/client';
 var jsxHeading = <h1 id="jxsHeading">Namaste react using jsx</h1>
 
 var heading = React.createElement("h1",{id:"heading"},"Namaste React");
+
+const Title = () => (<h4>Title</h4>)
+const HeadingCOmponent = () =>
+        ( 
+        <div>
+           <h1 id="fnHeading">Hello from funcitonal component 🚀</h1>
+           <Title/>
+        </div>
+        )
 //console.log(heading);
         var root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(jsxHeading);
+        root.render(<HeadingCOmponent/>);
