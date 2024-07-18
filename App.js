f269c7460fbaacb39e86864deed0ab9cdbd1a936
roopa26 <roopashree65@gmail.com>
2024-07-17 12:05:53 -1200

    food app created header component

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-var jsxHeading = <h1 id="jxsHeading">Namaste react using jsx</h1>
+const Header = () => {
+      return (<div className="header">
+                <div className='logo'>
+                        <img src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg" alt="logo"/>
+                </div>
+                <div className='nav-items'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                        <li>Cart</li>
+                </div>
+        </div>)
 
-var heading = React.createElement("h1",{id:"heading"},"Namaste React");
+}
 
-const Title = () => (<h4>Title</h4>)
-const HeadingCOmponent = () =>
-        ( 
-        <div>
-           <h1 id="fnHeading">Hello from funcitonal component 🚀</h1>
-           <Title/>
-        </div>
-        )
-//console.log(heading);
-        var root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(<HeadingCOmponent/>);
+const AppLayout = () =>{
+       return(<div className='app'>
+                <Header/>
+        </div>)
+}
+
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
