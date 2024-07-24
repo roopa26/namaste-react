1c7389da97143903db1256cb1e17fb00486153ce
roopa26 <roopashree65@gmail.com>
2024-07-24 10:41:46 -1200

    adding filter button to filter the cards based on rating using useState hook as state variable

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import resArray from "../../utils/mockData";
 import RestroCard from "./RestaurandCard";
 
 const Body = () => {
+    const [listOfRestaurant, setListOfRestaurant] = useState(resArray.data);
+    const handleBtnClick = ()=>{
+        const filteredData = listOfRestaurant.filter((res)=> res.rating.score > 4.4)
+        setListOfRestaurant(filteredData);
+    }
     return(
     <div className='body'>
              <div className='search'>
                      <input type='text' placeholder='type here to search you favorites food'/>
              </div>
+             <div>
+                <button onClick={handleBtnClick}>Filter Resstaurant</button>
+             </div>
              <div className='res-container'>
                     {
-                     resArray.data.map((obj)=> <RestroCard resObj = {obj}/>)
+                     listOfRestaurant.map((obj)=> <RestroCard key={obj.restaurantCode} resObj = {obj}/>)
                     }
              </div>
     </div>
