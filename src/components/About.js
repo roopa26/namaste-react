2ed9ca45cf4185800159beea87c62bb66d9360df
roopa26 <roopashree65@gmail.com>
2024-08-26 08:55:36 -1200

    removing unnecessary code regarding class based component

--- src/components/About.js
+++ src/components/About.js
@@ -2,35 +2,34 @@ import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-// const About = () => {
-//     return(<div>
-//         <h3>This is about the food app created when learning React Web Series.</h3>
-//         <User name={"Roopa"} location={"Namma Kudla"}/>
-//         <UserClass name={"Roopa"} location={"Namma Kudla"}/>
-//     </div>)
-// }
+const About = () => {
+    return(<div>
+        <h3>This is about the food app created when learning React Web Series.</h3>
+        <User name={"Roopa"} location={"Namma Kudla"}/>
+    </div>)
+}
 
 // export default About;
 
-class About extends React.Component{
-    constructor(props){
-        super(props)
-        console.log("parent component constructor")
-    }
+// class About extends React.Component{
+//     constructor(props){
+//         super(props)
+//         console.log("parent component constructor")
+//     }
 
-    componentDidMount(){
-        console.log("parent component did mount")
-    }
+//     componentDidMount(){
+//         console.log("parent component did mount")
+//     }
 
-    render(){
-        console.log("parent component render")
-        return(<div>
-                <h3>This is about the food app created when learning React Web Series.</h3>
-                <User name={"Roopa"} location={"Namma Kudla"}/>
-                <UserClass name={"Roopa"} location={"Namma Kudla"}/>
-                <UserClass name={"Roopa1"} location={"Namma Kudla"}/>
-               </div>)
-    }
-}
+//     render(){
+//         console.log("parent component render")
+//         return(<div>
+//                 <h3>This is about the food app created when learning React Web Series.</h3>
+//                 <User name={"Roopa"} location={"Namma Kudla"}/>
+//                 <UserClass name={"Roopa"} location={"Namma Kudla"}/>
+//                 <UserClass name={"Roopa1"} location={"Namma Kudla"}/>
+//                </div>)
+//     }
+// }
 
 export default About
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,35 +1,35 @@
-import React from "react";
+// import React from "react";
 
-export default class UserClass extends React.Component{
-    constructor(props){
-        console.log(props.name+" child component constructor")
-        super(props);
-        this.state = {
-            count:0,
-            count2:1
-        }
+// export default class UserClass extends React.Component{
+//     constructor(props){
+//         console.log(props.name+" child component constructor")
+//         super(props);
+//         this.state = {
+//             count:0,
+//             count2:1
+//         }
     
-    }
+//     }
 
-    componentDidMount(){
-        console.log(this.props.name+" child component did mount")
-    }
+//     componentDidMount(){
+//         console.log(this.props.name+" child component did mount")
+//     }
 
-    render(){
-        console.log(this.props.name+" child component render")
-        const {count,count2} = this.state;
-        return(
-        <div className="user-card">
-             <h1>{count}</h1>
-             <h1>{count2}</h1>
-             <button onClick = {()=>{this.setState({count : this.state.count+1,
-                count2 : this.state.count2+1
-             })
-             }}>Increase Count</button>
-             <h4>Name : {this.props.name}</h4>
-             <h4>Location : {this.props.location}</h4>
-             <h5>Contact : @roopashetty</h5>
-        </div>     
-        )
-    }
-}
+//     render(){
+//         console.log(this.props.name+" child component render")
+//         const {count,count2} = this.state;
+//         return(
+//         <div className="user-card">
+//              <h1>{count}</h1>
+//              <h1>{count2}</h1>
+//              <button onClick = {()=>{this.setState({count : this.state.count+1,
+//                 count2 : this.state.count2+1
+//              })
+//              }}>Increase Count</button>
+//              <h4>Name : {this.props.name}</h4>
+//              <h4>Location : {this.props.location}</h4>
+//              <h5>Contact : @roopashetty</h5>
+//         </div>     
+//         )
+//     }
+// }
