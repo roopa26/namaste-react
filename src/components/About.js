3f200fe261d596d80151047aba85d85f11f156fe
roopa26 <roopashree65@gmail.com>
2024-08-26 08:52:10 -1200

    learning class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,36 @@
-const About = () => {
-    return(<div>
-        About Page
-    </div>)
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+// const About = () => {
+//     return(<div>
+//         <h3>This is about the food app created when learning React Web Series.</h3>
+//         <User name={"Roopa"} location={"Namma Kudla"}/>
+//         <UserClass name={"Roopa"} location={"Namma Kudla"}/>
+//     </div>)
+// }
+
+// export default About;
+
+class About extends React.Component{
+    constructor(props){
+        super(props)
+        console.log("parent component constructor")
+    }
+
+    componentDidMount(){
+        console.log("parent component did mount")
+    }
+
+    render(){
+        console.log("parent component render")
+        return(<div>
+                <h3>This is about the food app created when learning React Web Series.</h3>
+                <User name={"Roopa"} location={"Namma Kudla"}/>
+                <UserClass name={"Roopa"} location={"Namma Kudla"}/>
+                <UserClass name={"Roopa1"} location={"Namma Kudla"}/>
+               </div>)
+    }
 }
 
-export default About;
+export default About
---
+++ src/components/User.js
@@ -0,0 +1,8 @@
+
+export default User = ({name,location}) => {
+    return(<div className="user-card">
+        <h4>Name : {name}</h4>
+        <h4>Location : {location}</h4>
+        <h5>Contact : @roopashetty</h5>
+    </div>)
+}
